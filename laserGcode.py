4734b55eae2d4bc7d227f8df2e036b7f0e4448b7
ikumar21 <ikumar@scu.edu>
2023-04-01 14:38:17 -0700

    improved gcode

--- laserGcode.py
+++ laserGcode.py
@@ -6,14 +6,6 @@ import threading
 '''windows'''
 #laserDexarm = Dexarm(port="COM4")
 
-def getXY(stringXY,xArray,yArray):
-    startIndexX = stringXY.index('X')+1;
-    stopIndexX = stringXY.index(' ', 3)
-    startIndexY = stringXY.index('Y')+1;
-    xArray.append(float(stringXY[startIndexX:stopIndexX]))
-    yArray.append(float(stringXY[startIndexY:]))
-    return float(stringXY[startIndexX:stopIndexX]),float(stringXY[startIndexY:]);
-
 
 def editHeaderAddNewLines(lines):
     #Adds in new lines
@@ -33,27 +25,25 @@ def gcode_creation(message,specifiedLength,fixHeight,power):
     power = int(power) if (int(power) <256 and int(power)>0) else 100
     lines = ttg(message,2,0,"Return",400).toGcode("M3 S"+str(power),"M5","G1","G1")#Generate Laser Gcode
     editHeaderAddNewLines(lines)
-    xPos, yPos = [],[]
-    for line in lines:#Inserts all x/y position into arrays
-        if(line[0]=='G' and (line[1]=='1' or line[1]=='0') and line[3]=='X'):
-            getXY(line,xPos,yPos)
-
+    #Get all movement x, y positions:
+    xPos = [float(line[4:line.index(' ', 3)]) for line in lines if line[0]=='G' and line[3]=='X']
+    yPos = [float(line[line.index('Y')+1:]) for line in lines if line[0]=='G' and line[3]=='X']
     if(fixHeight==True):#Scales according to height or width
         scalar = float(specifiedLength)/(max(yPos)-min(yPos))
     else:
         scalar=float(specifiedLength)/(max(xPos)-min(xPos))
-    for linePos in range(len(lines)):
+    lineIndex =0;
+    for linePos in range(len(lines)):#changes Gcode to scale size
         line = lines[linePos]
         if(line[0]=='G' and (line[1]=='1' or line[1]=='0') and line[3]=='X'):
-            x, y = getXY(line,xPos,yPos)
-            lines[linePos] = line[0:4] + str(round(scalar*x,2))+" Y"+str(round(scalar*y,2))+'\n'     
-
+            lines[linePos] = line[0:4] + str(round(scalar*xPos[lineIndex],2))+" Y"+str(round(scalar*yPos[lineIndex],2))+'\n'     
+            lineIndex+=1
     with open("outputGcode.txt", "w") as f:#Write to file
         f.writelines(lines)
     f.close()
 
 if __name__ == "__main__":
-    gcode_creation("ABC",10.2,True,-32)
+    gcode_creation("BCA",115,True,34)
     # laserDexarm.go_home()
     # laserDexarm._send_cmd("G92.1\r\n")
     # laserDexarm.set_workorigin()

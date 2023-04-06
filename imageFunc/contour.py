efcfdbda1e410bfac7f1c857d8352e9902c80882
ikumar21 <ikumar@scu.edu>
2023-04-06 12:06:44 -0700

    all

--- imageFunc/contour.py
+++ imageFunc/contour.py
@@ -3,34 +3,54 @@ import numpy as np
 import math
 import undistort
 import imutils
-
+from statistics import mean
+import matplotlib.pyplot as plt
 
 
 img_path = "testImages/17.jpg"
 img = cv2.imread(img_path)
-
 imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-undistortedImg = cv2.imread(img_path) 
-grayImg = cv2.cvtColor(undistortedImg, cv2.COLOR_BGR2GRAY)
-gs_img = cv2.GaussianBlur(grayImg, (5, 5), 0) 
-threshA = cv2.threshold(gs_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-dimensions = threshA.shape
-print(dimensions)
+dimensions = img.shape#Get dimensions of image to eliminate small contours
+
+def threshPic(undistortedImg):
+    undistortedImg = cv2.imread(img_path) 
+    grayImg = cv2.cvtColor(undistortedImg, cv2.COLOR_BGR2GRAY)
+    gs_img = cv2.GaussianBlur(grayImg, (5, 5), 0) 
+    threshA = cv2.threshold(gs_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    return threshA;
 
 
-contours, hierarchy = cv2.findContours(image=threshA, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-threshA = cv2.cvtColor(threshA,cv2.COLOR_GRAY2BGR)
-cv2.imshow('noC', threshA)
+
+
+
+
+thresholdPic = threshPic(cv2.imread(img_path))
+threshA = thresholdPic
+cv2.imshow('noC', thresholdPic)
+
+
+
+
 
 # print(contours)
 # print("=" * 80)
 # print(contours[0])
 # print("=" * 80)
 # print(contours[1])
+
+contours, hierarchy = cv2.findContours(image=threshA, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+threshA = cv2.cvtColor(threshA,cv2.COLOR_GRAY2BGR)
 centerX =[]
 centerY = []
+
+
 for c in contours:
     x,y,w,h = cv2.boundingRect(c)
+    xs = [v[0][0] for v in c]
+    ys = [dimensions[0]-v[0][1] for v in c]
+    # plt.plot(xs,ys) 
+    # plt.grid()
+    # plt.show()
     if(w*h<0.95*dimensions[0]*dimensions[1]and w*h>0.0003*dimensions[0]*dimensions[1]):#Don't plot if too big or too small
         print("Area",w*h)
         print("=" * 80)
@@ -61,33 +81,36 @@ for i in range(len(contours)):
     objectLocations.append(pts)
     print("=" * 80)
     print(pts)
-    cv2.putText(undistortedImg, "{0}".format(i+1), (centerX[i], centerY[i]),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0),2)
+    cv2.putText(img, "{0}".format(i+1), (centerX[i], centerY[i]),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0),2)
 
+cv2.imshow('original', img)
 
 for i in range(len(centerX)):
     print("Object {0} Center: x={1} ; y ={2}".format(i+1,centerX[i],centerY[i]))
 
 
-cv2.imshow('original', undistortedImg)
+
+
+def meanHSV(yPixelLocationsArr,xPixelLocationsArr, imgHSV):
+    #imgHSV[yPixelLocation][xPixelLocation]= [H,S,V]
+    avgH = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][0]) for indexPixel in range(len(yPixelLocationsArr))])
+    avgS = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][1]) for indexPixel in range(len(yPixelLocationsArr))])
+    avgV = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][2]) for indexPixel in range(len(yPixelLocationsArr))])
+    return [avgH,avgS,avgV]
 
 valuesHSV = []
-for object in objectLocations:
-    avgH =0.0;
-    avgS =0.0;
-    avgV =0.0;
-    for indexPixel in range(len(object)):
-        avgH = (1.0*avgH*(indexPixel)+1.0*imgHSV[object[0][indexPixel]][object[1][indexPixel]][0])/(indexPixel+1.0);
-        avgS = (1.0*avgS*(indexPixel)+1.0*imgHSV[object[0][indexPixel]][object[1][indexPixel]][1])/(indexPixel+1.0);
-        avgV = (1.0*avgV*(indexPixel)+1.0*imgHSV[object[0][indexPixel]][object[1][indexPixel]][2])/(indexPixel+1.0);
-    valuesHSV.append([avgH,avgS,avgV])
-    print("Average HSV:{0},{1},{2}".format(avgH,avgS,avgV))
+for object in objectLocations:#Find HSV in pixel locations
+    # #object = [[Array of all y pixel locations in contour], [Array " " x " "], [Array of all 0s]]
+    valuesHSV.append(meanHSV(object[0],object[1], imgHSV=imgHSV))
+
+def correctHSV(hsvArray):
+    #Returns tuple : (H,S,V) in correct format: 0-360degrees, 0-100%, 0-100%
+    return round(hsvArray[0]*2.0), round(100.0*hsvArray[1]/255.0,1), round(100.0*hsvArray[2]/255.0,1)
+
 
 print("=" * 80)
 for objectHSV in valuesHSV:
-    avgH=objectHSV[0];
-    avgS=objectHSV[1];
-    avgV=objectHSV[2]
-    print("Average HSV(Corrected):{0},{1},{2}".format(round(avgH*2.0),round(100.0*avgS/255.0,1),round(100.0*avgV/255.0,1)))
+    print("Average HSV(Corrected):{0},{1},{2}".format(*correctHSV(objectHSV)))
 
 cv2.imshow('thresholdA', threshA)
 while True:
---
+++ imageFunc/test.py
@@ -0,0 +1,4 @@
+def fa():
+    return 2,4,5;
+
+print("{0},{1},{2}".format(*fa()))

afe343008cdac3a6ca6a8da1530a5100a6239eef
ikumar21 <ikumar@scu.edu>
2023-04-15 11:01:37 -0700

    laser

--- imageFunc/contour.py
+++ imageFunc/contour.py
@@ -5,11 +5,63 @@ import undistort
 from statistics import mean
 # import matplotlib.pyplot as plt
 
+img_path = "testImages/20.jpg"
 
-img_path = "testImages/17.jpg"
-img = cv2.imread(img_path)
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-dimensions = img.shape#Get dimensions of image to eliminate small contours
+
+def correctHSV(hsvArray):
+    #Returns tuple : (H,S,V) in correct format: 0-360degrees, 0-100%, 0-100%
+    return round(hsvArray[0]*2.0), round(100.0*hsvArray[1]/255.0,1), round(100.0*hsvArray[2]/255.0,1)
+
+def meanHSV(yPixelLocationsArr,xPixelLocationsArr, imgHSV):#Returns mean HSV in area
+    #imgHSV[yPixelLocation][xPixelLocation]= [H,S,V]
+    avgH = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][0]) for indexPixel in range(len(yPixelLocationsArr))])
+    avgS = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][1]) for indexPixel in range(len(yPixelLocationsArr))])
+    avgV = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][2]) for indexPixel in range(len(yPixelLocationsArr))])
+    return [avgH,avgS,avgV]
+
+
+def allHSV(yPixelLocationsArr,xPixelLocationsArr, imgHSV):#Returns all HSV and their occurence
+    #Input yPixelLocationsArr : [yPos0, yPos1, ..]
+    #Input xPixelLocationsArr : [xPos0, xPos1, ..]
+    #Input imgHSV: image in HSV
+
+    #Returns [[H0,S0,V0],[H1,S1,V1], ...], [count0, count1, ...]
+    #imgHSV[yPixelLocation][xPixelLocation]= [H,S,V]
+    allHSV = []
+    countHSV = []
+    for indexPixel in range(len(yPixelLocationsArr)):
+        hsvValue = imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]];
+        try:
+            indexInHSV = allHSV.index(hsvValue.tolist())
+        except:
+            indexInHSV = -1
+        if indexInHSV ==-1:#HSV value not in array
+            allHSV.append(hsvValue.tolist())
+            countHSV.append(1)
+        else:#Increment count
+            countHSV[indexInHSV]+=1
+
+    return allHSV,countHSV
+
+
+
+def pixelsInContour(contours,img, emptyImg):
+    objectLocations = []
+    # For each list of contour points...
+    for i in range(len(contours)):
+        # Create a mask image that contains the contour filled in
+        cimg = np.zeros_like(img)
+
+        #cimg = np.full((1080, 1920), 0, dtype=np.int32)
+        
+        cv2.drawContours(cimg, contours, i, color=255, thickness=-1)
+        #cv2.imshow("object{}".format(i+1),cimg)
+        # Access the image pixels and create a 1D numpy array then add to list
+        pts = np.where(cimg == 255)
+        #cv2.drawContours(emptyImg, contours, i, color=0, thickness=-1)#reset
+        objectLocations.append(pts)
+    return objectLocations
+
 
 def threshPic(undistortedImg):
     grayImg = cv2.cvtColor(undistortedImg, cv2.COLOR_BGR2GRAY)
@@ -18,102 +70,78 @@ def threshPic(undistortedImg):
     return threshA;
 
 
+def getDrawContour(imgThreshBGR,imgThreshGray):
+    contours, hierarchy = cv2.findContours(image=imgThreshGray, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)#Find contours
+    dimensions = imgThreshBGR.shape#Find pixel dimensions
+    centerX =[]#x location of each object's center
+    centerY = []#y location of each object's center
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)#Get Width and Height of each object
+        #Get Contour x/y end points
+        # xs = [v[0][0] for v in c] 
+        # ys = [dimensions[0]-v[0][1] for v in c]
+        # # plt.plot(xs,ys) 
+        # # plt.grid()
+        # # plt.show()
+        if(w*h<0.95*dimensions[0]*dimensions[1]and w*h>0.003*dimensions[0]*dimensions[1]):#Don't plot if too big or too small
+            cv2.drawContours(imgThreshBGR, [c], -1, (0, 255, 0), 2)#Draw Contours in Green
+            M = cv2.moments(c)
+            if M["m00"] != 0:#Found Center, don't divide by 0
+                print("FOUND, Area:",w*h)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                centerX.append(cX)
+                centerY.append(cY)
+                cv2.circle(imgThreshBGR, (cX, cY), 7, (0, 0, 255), -1)
+                cv2.putText(imgThreshBGR, "center", (cX - 20, cY - 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            # draw the contour and center of the shape on the image
+
+    return centerX,centerY, contours
 
 
 
+if __name__ == "__main__":
+    objectWhite= False;
+    imgBGR = cv2.imread(img_path)
+    imgHSV = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2HSV)
+    dimensions = imgBGR.shape#Get dimensions of image to eliminate small contours
+    simpleImage = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2GRAY)
+    emptyImg = np.zeros_like(imgBGR)
 
-thresholdPic = threshPic(cv2.imread(img_path))
-threshA = thresholdPic
-cv2.imshow('noC', thresholdPic)
+    #imgBGR = undistort.undistort(img, showImage=False) 
 
+    thresholdPic = threshPic(imgBGR)
+    if(objectWhite):#If Objects are white, invert so it becomes black background/white objects
+        thresholdPic = cv2.bitwise_not(thresholdPic)#Invert Image
 
+    threshColor = cv2.cvtColor(thresholdPic,cv2.COLOR_GRAY2BGR)#Convert Gray image to BGR to have Markings in color
 
-
-
-# print(contours)
-# print("=" * 80)
-# print(contours[0])
-# print("=" * 80)
-# print(contours[1])
-
-contours, hierarchy = cv2.findContours(image=threshA, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-threshA = cv2.cvtColor(threshA,cv2.COLOR_GRAY2BGR)
-centerX =[]
-centerY = []
-
-
-for c in contours:
-    x,y,w,h = cv2.boundingRect(c)
-    xs = [v[0][0] for v in c]
-    ys = [dimensions[0]-v[0][1] for v in c]
-    # plt.plot(xs,ys) 
-    # plt.grid()
-    # plt.show()
-    if(w*h<0.95*dimensions[0]*dimensions[1]and w*h>0.0003*dimensions[0]*dimensions[1]):#Don't plot if too big or too small
-        print("Area",w*h)
-        print("=" * 80)
-        cv2.drawContours(threshA, [c], -1, (0, 255, 0), 2)
-        M = cv2.moments(c)
-        if M["m00"] != 0:#Found Center
-            print("FOUND")
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            centerX.append(cX)
-            centerY.append(cY)
-            cv2.circle(threshA, (cX, cY), 7, (0, 0, 255), -1)
-            cv2.putText(threshA, "center", (cX - 20, cY - 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        # draw the contour and center of the shape on the image
-        cv2.drawContours(threshA, [c], -1, (0, 255, 0), 2)
-        print(img[cY][cX])
-
-objectLocations = []
-# For each list of contour points...
-for i in range(len(contours)):
-    # Create a mask image that contains the contour filled in
-    cimg = np.zeros_like(img)
-    cv2.drawContours(cimg, contours, i, color=255, thickness=-1)
-    #cv2.imshow("object{}".format(i+1),cimg)
-    # Access the image pixels and create a 1D numpy array then add to list
-    pts = np.where(cimg == 255)
-    objectLocations.append(pts)
-    print("=" * 80)
-    print(pts)
-    cv2.putText(img, "{0}".format(i+1), (centerX[i], centerY[i]),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0),2)
-
-cv2.imshow('original', img)
-
-for i in range(len(centerX)):
-    print("Object {0} Center: x={1} ; y ={2}".format(i+1,centerX[i],centerY[i]))
+    centerX,centerY, contours = getDrawContour(imgThreshBGR=threshColor,imgThreshGray=thresholdPic)
+    objectLocations = pixelsInContour(contours,simpleImage,emptyImg)
 
 
+    for object in objectLocations:
+        #print(meanHSV(object[0],object[1], imgHSV))
+        hsv, count = allHSV(object[0],object[1], imgHSV)
+        print(hsv,count)
+        print("="*80)
+        # b = [hsv[0].tolist()]
+        # try:
+        #     indexInHSV = b.index(hsv[1].tolist())
+        # except:
+        #     indexInHSV = -1
+        # if(indexInHSV ==-1):
+        #     print("OK")
+
+    cv2.imshow('thresh', threshColor)
+    cv2.imshow('src_img', imgBGR)
+
+    while True:
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
 
 
-def meanHSV(yPixelLocationsArr,xPixelLocationsArr, imgHSV):
-    #imgHSV[yPixelLocation][xPixelLocation]= [H,S,V]
-    avgH = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][0]) for indexPixel in range(len(yPixelLocationsArr))])
-    avgS = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][1]) for indexPixel in range(len(yPixelLocationsArr))])
-    avgV = mean([float(imgHSV[yPixelLocationsArr[indexPixel]][xPixelLocationsArr[indexPixel]][2]) for indexPixel in range(len(yPixelLocationsArr))])
-    return [avgH,avgS,avgV]
-
-valuesHSV = []
-for object in objectLocations:#Find HSV in pixel locations
-    # #object = [[Array of all y pixel locations in contour], [Array " " x " "], [Array of all 0s]]
-    valuesHSV.append(meanHSV(object[0],object[1], imgHSV=imgHSV))
-
-def correctHSV(hsvArray):
-    #Returns tuple : (H,S,V) in correct format: 0-360degrees, 0-100%, 0-100%
-    return round(hsvArray[0]*2.0), round(100.0*hsvArray[1]/255.0,1), round(100.0*hsvArray[2]/255.0,1)
-
-
-print("=" * 80)
-for objectHSV in valuesHSV:
-    print("Average HSV(Corrected):{0},{1},{2}".format(*correctHSV(objectHSV)))
-
-cv2.imshow('thresholdA', threshA)
-while True:
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
--- laserFunc/laserGcode.py
+++ laserFunc/laserGcode.py
@@ -1,10 +1,13 @@
-from pydexarm import Dexarm
+# from pydexarm import Dexarm
 import time
 import math 
 from ttgLib.TextToGcode import ttg
 import threading
 '''windows'''
 #laserDexarm = Dexarm(port="COM4")
+#Simulate G code: https://nraynaud.github.io/webgcode/
+class LaserCutterPrp:
+    a = 0;
 
 
 def editHeaderAddNewLines(lines):
@@ -14,18 +17,23 @@ def editHeaderAddNewLines(lines):
     #Changes Gcode Header to work for the dexarm
     lines[0]="G0 Z0\n"
     lines[1]="M2000\n"
-    lines[2]="M888 P1\nM888 P14 \nM5\n"
-    lines[3]="G0 F400\nG1 F400 \nG0 X0 Y0\n"
+    lines[2]="M888 P1\nM888 P14 \nM5\n"#M5 turns off Laser
+    lines[3]="G0 F400\nG1 F400 \n"
 
-def gcode_creation(message,specifiedLength,fixHeight,power):
+
+def gcode_message_creation(message,specifiedLength,fixHeight,power, messageCenter):
     #First argument - Message to Write
     #Second Argument - length desired (mm) 
-    #Third argument - True - height should be set to the length, False- width should be set
+    #Third argument - True - height should be set to the specified length, False- width should be set
     #4th Argument - power desired (0-255), default = 100 
-    
+    #5th Argument - center of object to be written (x,y)
+
     #Find power of laser cutter
     power = int(power) if (int(power) <256 and int(power)>0) else 100
-    lines = ttg(message,2,0,"Return",400).toGcode("M3 S"+str(power),"M5","G1","G1")#Generate Laser Gcode
+
+    #Generate Laser Gcode using library
+    # M5 turns off laser, M3 S100 turns on laser with power lvl 100/255
+    lines = ttg(message,2,0,"Return",400).toGcode("M3 S"+str(power),"M5","G1","G1")
     editHeaderAddNewLines(lines)#Fix up Header and add "\n"
 
     #Get all movement x, y positions:
@@ -37,13 +45,27 @@ def gcode_creation(message,specifiedLength,fixHeight,power):
         scalar = float(specifiedLength)/(max(yPos)-min(yPos))
     else:
         scalar=float(specifiedLength)/(max(xPos)-min(xPos))
-    lineIndex =0;
     
-    #changes Gcode to scale size
+    xPos = [elem*scalar for elem in xPos]
+    yPos = [elem*scalar for elem in yPos];
+
+    #Shift message to desired centerpoint
+    xShift = messageCenter[0]-(max(xPos)+min(xPos))/2.0
+    yShift = messageCenter[1]-(max(yPos)+min(yPos))/2.0
+    xPos = [elem+xShift for elem in xPos];
+    yPos = [elem+yShift for elem in yPos];
+
+    #Start at right place
+    lines[3]+="G1 X"+str(round(xPos[0],2))+" Y"+str(round(yPos[0],2))+"\n"
+
+    #Changes the Gcode x,y to scale/move, round to 2 decimal points (mm)
+    lineIndex =0;
     for linePos in range(len(lines)):
         line = lines[linePos]
         if(line[0]=='G' and (line[1]=='1' or line[1]=='0') and line[3]=='X'):
-            lines[linePos] = line[0:4] + str(round(scalar*xPos[lineIndex],2))+" Y"+str(round(scalar*yPos[lineIndex],2))+'\n'     
+            xPosString = str(round(xPos[lineIndex],2))
+            yPosString = str(round(yPos[lineIndex],2))
+            lines[linePos] = line[0:4] + xPosString+" Y"+yPosString+'\n'     
             lineIndex+=1
 
     #Write to file
@@ -51,14 +73,48 @@ def gcode_creation(message,specifiedLength,fixHeight,power):
         f.writelines(lines)
     f.close()
 
+
+def gcode_point_creation(coorArray,power):
+    #Input coorArray: array of arrays [x,y, LaserOn]
+    #LaserOn - should laser be on when going to next point
+    #Ex: [[0,0,True],[0,20,False], [10,20,True], [10,0,False/True]]
+    #Will create two vertical lines at x = 0 and 10 with height of 20.
+
+    lines = [" "," "," "," "]
+    editHeaderAddNewLines(lines)#Add header 
+    power = int(power) if (int(power) <256 and int(power)>0) else 100
+    laserOnCommand = "M3 S"+str(power)+"\n"
+    lastLaserCommand = False;
+    for coordinate in coorArray:
+        posCommand = "G"+str(int(lastLaserCommand))+ " X"+str(coordinate[0])+ " Y"+str(coordinate[1])+"\n"
+        lines.append(posCommand)
+        if(coordinate[2]!= lastLaserCommand):#Turn on/off laser if it isn't already
+            laserCommand = laserOnCommand if coordinate[2] else "M5\n"
+            lines.append(laserCommand)
+            lastLaserCommand=coordinate[2]; 
+    #for lines 
+    with open("outputGcode.txt", "w") as f:
+        f.writelines(lines)
+    f.close()
+
 if __name__ == "__main__":
-    gcode_creation("BCA",115,True,34)
+    center = (20,30);
+    #gcode_message_creation("A1",specifiedLength=30,fixHeight=True,power=34,messageCenter=center)
+    cArray = [[0,0,True],[20,20,True], [40,0,True], [0,0,False/True]]
+    gcode_point_creation(cArray,255)
     # laserDexarm.go_home()
     # laserDexarm._send_cmd("G92.1\r\n")
-    # laserDexarm.set_workorigin()
+    # #laserDexarm.set_workorigin() 
     # time.sleep(3)
     # with open("outputGcode.txt") as f:#Send gcode of the letters:
     #     lines = f.readlines()
     # for x in lines:
     #     a = x+"\r\n"
     #     laserDexarm._send_cmd(a);
+
+"""
+G0 X-11.50 Y-11.50
+M3 S127
+G1 X11.50 Y-11.50
+M5
+"""

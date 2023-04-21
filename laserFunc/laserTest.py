2ac8208e95b3485a08912b6c8519b910c8c73340
ikumar21 <ikumar@scu.edu>
2023-04-20 18:51:19 -0700

    laser

--- laserFunc/laserTest.py
+++ laserFunc/laserTest.py
@@ -9,6 +9,7 @@ import math
 from ttgLib.TextToGcode import ttg
 import threading
 import laserGcode as lgs
+import math
 
 #Before calling any laser functions, make sure the laser is off already
 
@@ -25,13 +26,21 @@ class Rectangle:
             self.angle = 0;
         else:
              self.angle = angle
-        self.center = center;
-        self.width = recWidth;
-        self.height = recHeight;
-        self.stepSize = stepSize;
+        self.center = [float(center[0]),float(center[1])];
+        self.width = float(recWidth);
+        self.height = float(recHeight);
+        self.stepSize = float(stepSize);
 
 
-
+class Triangle:#Equal Sides 
+    def __init__(self,center,sideLength,stepSize, angle =None ):
+        if angle is None:#Default-no Rotation(0); 0-120 degrees
+            self.angle = 0.0;
+        else:
+             self.angle = float(angle)
+        self.center = [float(center[0]),float(center[1])];
+        self.length = float(sideLength);
+        self.stepSize = float(stepSize);
 
 
 def rectangleOutlineLaser(cArray, rec: Rectangle):
@@ -52,7 +61,21 @@ def lineLaser(cArray,point1,point2):
     cArray.append([point2[0],point2[1], False])
 
 
-def rectangleFillLaser(cArray,rect: Rectangle):
+def rotate_coordinates(coordinates,angle,center):
+    cosA = math.cos(math.pi*angle/180.0)*1.0
+    sinA = math.sin(math.pi*angle/180.0)*1.0 
+    xCenter = center[0]
+    yCenter = center[1]
+
+    for coordinate in coordinates:#Necessary to round! otherwise 2.3000e-16 means 2.3 mm
+        xOld =  coordinate[0]-xCenter*1.0;
+        yOld = coordinate[1]-yCenter*1.0;
+        coordinate[0]= round(1.0*xOld*cosA+1.0*yOld*sinA+xCenter,2)
+        coordinate[1]= round(1.0*yOld*cosA-1.0*xOld*sinA+yCenter,2)
+
+
+
+def rectangleFillLaser(cArray:list,rect: Rectangle):
     #Input center: (x_center,y_center)
 
     yHeight = rect.center[1]+rect.height/2.0;#Start at top of rectangle
@@ -61,17 +84,20 @@ def rectangleFillLaser(cArray,rect: Rectangle):
     
     #Alternate between starting at the left and right of rectangle after each pass, More efficient
     alternateVar = True
-
+    newCoor = [];
     while(yHeight>=rect.center[1]-rect.height/2.0):#Stop at the bottom
         #Go to start of cut, and signal that next movement will be be laser cutting:
-        cArray.append([xStart*alternateVar+xStop*(not alternateVar),yHeight, True])
+        newCoor.append([xStart*alternateVar+xStop*(not alternateVar),yHeight, True])
 
         #Go to end of cut(laser is on);  Turn off laser
-        cArray.append([xStop*alternateVar+xStart*(not alternateVar),yHeight, False])
+        newCoor.append([xStop*alternateVar+xStart*(not alternateVar),yHeight, False])
 
         alternateVar=not alternateVar;#laser moved ends, so now start at the new end
-        yHeight-=rect.stepSize;#Go down a step level 
+        yHeight-=rect.stepSize;#Go down a step level
     
+    rotate_coordinates(newCoor,rect.angle,rect.center)
+    for elem in newCoor: cArray.append(elem)
+
     #Make sure to put False for last command to turn off laser:
     cArray[-1][2]=False
 
@@ -140,9 +166,64 @@ def createSquareWaferBoard(coorArray,wafB: Wafer_Board):
         lineLaser(coorArray, [xStart2,y2],[xStop2,y2])
     #End of Wafer Board
 
+def triangleOutlineLaser(cArray, tri: Triangle):
+    #Input center: (x_center,y_center)
+    newCoor = [];
+    
+    #Start at right bottom
+    xTri = tri.center[0]+tri.length/2;#Leftmost point of triangle
+    yTri = tri.center[1]-tri.length/2.0/math.sqrt(3);#Bottom of triangle
+    newCoor.append([xTri,yTri, True])
+    
+    #Go to Top Middle
+    xTri-=tri.length/2
+    yTri += tri.length*math.sqrt(3)/2;
+    newCoor.append([xTri,yTri, True])
+
+    #Go to bottom Left
+    xTri-=tri.length/2
+    yTri -= tri.length*math.sqrt(3)/2;
+    newCoor.append([xTri,yTri, True])
+    
+    #Go to bottom Right
+    xTri+=tri.length
+    #Make sure to put False for last command to turn off laser:
+    newCoor.append([xTri,yTri, False])
+
+    rotate_coordinates(newCoor,tri.angle,tri.center)
+    for elem in newCoor: cArray.append(elem)
 
 
+def triangleFillLaser(cArray,tri: Triangle):
+    #Input center: (x_center,y_center)
 
+    xLocation = tri.center[0]-tri.length/2.0+tri.stepSize #Start at leftmost point of triangle
+   
+    yStart = tri.center[1]-tri.length/2.0/math.sqrt(3.0)
+
+    
+    #Alternate between starting at the top and bottom of triangle after each pass, More efficient
+    alter = True
+    newCoor = [];
+    while(xLocation<=tri.center[0]+tri.length/2.0):#Stop at the bottom
+        xRelative = xLocation-tri.center[0]+tri.length/2; 
+
+        yStop = yStart + min(xRelative,tri.length-xRelative)*math.sqrt(3)
+        #Go to start of cut, and signal that next movement will be be laser cutting:
+        newCoor.append([xLocation,yStart*alter+yStop*(not alter), True])
+
+        #(Laser is on) Go to end of cut; Then turn off laser
+        newCoor.append([xLocation,yStop*alter+yStart*(not alter), False])
+
+        alter=not alter;#laser moved ends, so now start at the new end
+        xLocation+=tri.stepSize;#Go right a step level
+    
+    rotate_coordinates(newCoor,tri.angle,tri.center)
+    for elem in newCoor: cArray.append(elem)
+    triangleOutlineLaser(cArray, tri)#Outline triangle for sharp edges
+
+    #Make sure to put False for last command to turn off laser:
+    cArray[-1][2]=False
 
 def arrangement1(coorArray,waferBoard:Wafer_Board):
 
@@ -150,15 +231,15 @@ def arrangement1(coorArray,waferBoard:Wafer_Board):
     createSquareWaferBoard(coorArray, waferBoard);
 
     #Get all centers of each square in board:
-    squareCenters = centerSquares(waferBoard,);#
+    squareCenters = centerSquares(waferBoard);#
     
     #Entries in Wafer
 
-    rectangleFillLaser(coorArray,Rectangle(squareCenters[0][0],10,10,0.4))
-    rectangleFillLaser(coorArray,Rectangle(squareCenters[0][2],10,10,0.3))
+    rectangleFillLaser(coorArray,Rectangle(squareCenters[0][0],10,10,0.4,45))
+    rectangleFillLaser(coorArray,Rectangle(squareCenters[0][2],10,10,0.3,30))
     rectangleFillLaser(coorArray,Rectangle(squareCenters[1][1],7,10,0.3))
     rectangleOutlineLaser(coorArray,Rectangle(squareCenters[1][3],5,10,None))
-    rectangleFillLaser(coorArray,Rectangle(squareCenters[2][0],5,10,0.25))
+    rectangleFillLaser(coorArray,Rectangle(squareCenters[2][0],5,10,0.25,15))
     rectangleFillLaser(coorArray,Rectangle(squareCenters[2][2],5,5,0.25))
     rectangleFillLaser(coorArray,Rectangle(squareCenters[3][0],10,5,0.25))
     rectangleFillLaser(coorArray,Rectangle(squareCenters[3][1],10,3,0.25))
@@ -168,13 +249,17 @@ def arrangement1(coorArray,waferBoard:Wafer_Board):
 
 
 
+
 if __name__ == "__main__":
     coordinateArray = []
     waferBoard1 = Wafer_Board((0,300),80,4)
 
-
-    arrangement1(coordinateArray, waferBoard1);
-
+    #rectangleFillLaser(coordinateArray,Rectangle([0,0],10,10,0.2,45))
+    #arrangement1(coordinateArray, waferBoard1);
+    tri1 = Triangle([20,10],10,0.1,30)
+    triangleFillLaser(coordinateArray,tri1)
+    tri2=Triangle([0,10],5,0.2,10)
+    triangleFillLaser(coordinateArray,tri2)
     lgs.gcode_point_creation(coordinateArray,200)#Create gcode with power level 200;
     #runLaser();
 

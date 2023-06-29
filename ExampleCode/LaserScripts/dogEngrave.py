126e973556654fa9862194f123ccbe0e37821f2c
rotricsrsl <129425453+rotricsrsl@users.noreply.github.com>
2023-06-29 12:44:34 -0700

    LL

--- ExampleCode/LaserScripts/dogEngrave.py
+++ ExampleCode/LaserScripts/dogEngrave.py
@@ -15,38 +15,35 @@ def EngraveActualDog(laserRobot):
     fileLocName = "LaserModule/rotricsGcode/dog.gcode";
     
     #Set laser object center, angle, height/width, laser power:
-    dog_laser = l_m.Laser_Object_Properties(fixHeight=True,centerPoint=[0,340-25],specifiedLength=50,laserPower=125,angle=0)
+    dog_laser = l_m.Laser_Object_Properties(fixHeight=True,centerPoint=[0,305],specifiedLength=20,laserPower=125,angle=0)
     
     #Generate G-Code:
-    l_m.GcodeObjectCreation(fileLocName,dog_laser)
+    width, height = l_m.GcodeObjectCreation(fileLocName,dog_laser)
 
     #Run the Laser
     l_m.runLaser(laserRobot)
 
 
+
 def EngraveLettersDog(laserRobot):
-    width, height = l_m.gcode_message_creation("DOG",20,True,125,(0,273))
+    width, height = l_m.gcode_message_creation("DOG",20,False,125,(0,290))
     l_m.runLaser(laserRobot)
     return width;
 
 def EngraveUnderline(laserRobot, width):
-    #Get lines from G-code:
-    with open("LaserModule/rotricsGcode/HorizontalLine.gcode", "r") as f: lineLines = f.readlines()
+
+    #File Name:
+    fileLocName = "LaserModule/rotricsGcode/HorizontalLine.gcode";
     
     #Set laser object center, angle, height/width, laser power:
-    line_properties = l_m.Laser_Object_Properties(fixHeight=False,centerPoint=[0,80],specifiedLength=width,laserPower=125,angle=0)
+    line_properties = l_m.Laser_Object_Properties(fixHeight=False,centerPoint=[0,285],specifiedLength=20,laserPower=200,angle=0)
     
-    #Get the modifed G-code with right properties
-    laserLines = l_m.ModifyGcode(lineLines, line_properties)
+    #Generate G-Code:
+    width, height = l_m.GcodeObjectCreation(fileLocName,line_properties)
 
-    #Write to file
-    with open("outputGcode.txt", "w") as f:
-        f.writelines(laserLines)
-    f.close()
-
+    print("WIDTH", width, height)
     #Run the Laser
     l_m.runLaser(laserRobot)
-
 
 
 if __name__ == "__main__":
@@ -56,6 +53,7 @@ if __name__ == "__main__":
     #Laser:
     EngraveActualDog(laserDexarm);
     letterWidth = EngraveLettersDog(laserDexarm)
+    letterWidth=6;
     EngraveUnderline(laserDexarm,letterWidth);
 
 
